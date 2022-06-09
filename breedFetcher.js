1aa5267d7842c6527c33a56aee94474f394c0609
Derine Banis <d.banis.13@gmail.com>
2022-06-09 15:31:32 -0700

    Implemented further functionality for async functions.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,30 @@
 const request = require('request');
-const process = require('process');
-const argv = process.argv.slice(2);
-
-const fetchBreed = (argv) => {
-  if (argv.length !== 0) {
-    const breed = argv.join('%20');
-    
-    request(`http://api.thescatapi.com/v1/breeds/search?name=${breed}`, (err,response,body) =>{
+//Assisted by Cal and Dan from East Coast May 30th, 2022 cohort.
+const fetchBreed = (breed, cb) => {
+  if (breed.length !== 0) {
+    breed = breed.join('%20');
+    request(`http://api.thecatapi.com/v1/breeds/search?name=${breed}`, (err,response,body) =>{
       //Returning Error Results
       if (err) {
-        return console.log(err);
+       return cb(err);
       }
       //For Returning Body Results
       body = JSON.parse(body);
       const catData = body[0];
       if (catData) {
         const catDesc = catData.description;
-        console.log(catDesc);
+        return cb(null, catDesc);
       } else {
-        console.log('Sorry! The cat was not found.');
+        return cb('Sorry! The cat was not found.');
       }
     });
   } else {// For handling case if user does not enter a cat to search for
-    console.log('Please enter a cat to search for.');
+    return cb('Please enter a cat to search for.');
   }
 };
-fetchBreed(argv);
 
 
+
+
+
+module.exports = {fetchBreed}
---
+++ index.js
@@ -0,0 +1,13 @@
+const process = require('process');
+const argv = process.argv.slice(2);
+
+const {fetchBreed} = require('./breedFetcher');
+
+fetchBreed(argv, (error, desc)=> {
+  if(error){
+    console.log(`Error:\n${error}`)
+  }
+  else {
+    console.log(desc)
+  }
+})
